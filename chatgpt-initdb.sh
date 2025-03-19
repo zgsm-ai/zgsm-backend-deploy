f7445514d0317d90c7e8acad36a59388343eb902
Liu Wei <monkeydluffy6017@gmail.com>
2025-03-19 18:02:46 +0800

    fix: update deploy script (#2)

--- chatgpt-initdb.sh
+++ chatgpt-initdb.sh
@@ -2,7 +2,7 @@
 
 docker compose up postgres -d
 sleep 5
-docker exec -it zgsm-compose-deploy-postgres-1 /usr/local/bin/psql -U keycloak -h127.0.0.1 -p5432 -c "CREATE DATABASE chatgpt;"
+docker exec -it zgsm-backend-deploy-postgres-1 /usr/local/bin/psql -U keycloak -h127.0.0.1 -p5432 -c "CREATE DATABASE chatgpt;"
 docker compose --profile initdb up chatgpt-initdb
 sleep 5
 docker compose down
